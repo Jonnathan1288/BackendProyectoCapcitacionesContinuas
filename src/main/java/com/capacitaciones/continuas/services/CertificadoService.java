90ba7dd32098b6a587a365c2a62dcd948441f127
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2023-04-23 13:52:27 -0500

    update
    update

--- src/main/java/com/capacitaciones/continuas/services/CertificadoService.java
+++
@@ -1,7 +0,0 @@
-package com.capacitaciones.continuas.services;
-
-import com.capacitaciones.continuas.models.Capacitador;
-import com.capacitaciones.continuas.models.Certificado;
-
-public interface CertificadoService extends  GenericService<Certificado, Integer>{
-}
