63d469c87896aa46e39a3d37dfcceefd2ee1a6c1
EstebanBacuilima <davidbacuilima13@gmail.com>
2023-04-22 20:38:32 -0500

    nuevos
    Cambios

--- src/main/java/com/capacitaciones/continuas/repositorys/EspecialidadRepository.java
+++ src/main/java/com/capacitaciones/continuas/repositorys/EspecialidadRepository.java
@@ -2,7 +2,9 @@ package com.capacitaciones.continuas.repositorys;
 
 import com.capacitaciones.continuas.models.Especialidad;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EspecialidadRepository extends JpaRepository<Especialidad, Integer> {
 }
 
--- src/main/java/com/capacitaciones/continuas/repositorys/FichaMatriculaRepository.java
+++ src/main/java/com/capacitaciones/continuas/repositorys/FichaMatriculaRepository.java
@@ -2,7 +2,9 @@ package com.capacitaciones.continuas.repositorys;
 
 import com.capacitaciones.continuas.models.FichaMatricula;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FichaMatriculaRepository extends JpaRepository<FichaMatricula, Integer> {
 }
 
