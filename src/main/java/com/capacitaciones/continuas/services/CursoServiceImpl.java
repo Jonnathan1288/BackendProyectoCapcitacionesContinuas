aa28eb65c54bdb010c12eead1ca8cc3765858009
EstebanBacuilima <davidbacuilima13@gmail.com>
2023-04-25 07:57:08 -0500

    new
    cambios

--- src/main/java/com/capacitaciones/continuas/services/CursoServiceImpl.java
+++ src/main/java/com/capacitaciones/continuas/services/CursoServiceImpl.java
@@ -1,8 +1,6 @@
 package com.capacitaciones.continuas.services;
 
-import com.capacitaciones.continuas.models.ControlHorario;
 import com.capacitaciones.continuas.models.Curso;
-import com.capacitaciones.continuas.repositorys.ControlHorarioRepository;
 import com.capacitaciones.continuas.repositorys.CursoRepositry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
