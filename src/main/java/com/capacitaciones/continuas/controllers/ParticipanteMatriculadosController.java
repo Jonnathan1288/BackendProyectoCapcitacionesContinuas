03df34144cd4eaa8cf35d40f190804e5177a568c
EstebanBacuilima <davidbacuilima13@gmail.com>
2023-04-26 20:31:56 -0500

    nuevos
    cambios

--- src/main/java/com/capacitaciones/continuas/controllers/ParticipanteMatriculadosController.java
+++ src/main/java/com/capacitaciones/continuas/controllers/ParticipanteMatriculadosController.java
@@ -1,5 +1,6 @@
 package com.capacitaciones.continuas.controllers;
 
+import com.capacitaciones.continuas.models.ParticipantesAprobados;
 import com.capacitaciones.continuas.models.PartipantesMatriculados;
 import com.capacitaciones.continuas.services.ParticipantesMatriculadosService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,5 +36,30 @@ public class ParticipanteMatriculadosController {
         }
     }
 
+    @GetMapping("/participantesMatriculados/findbyId/{id}")
+    public ResponseEntity<?> getPartipantesMatriculadosById(@PathVariable("id") Integer id){
+        try {
+            PartipantesMatriculados nc = participantesMatriculadosService.findById(id);
+            if(nc != null){
+                return new ResponseEntity<>(nc, HttpStatus.OK);
+            }
+            return new ResponseEntity<>("PARTICIPANTE APROBADOS NO ENCONTRADA",HttpStatus.NOT_FOUND);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
+    @PutMapping("/participantesMatriculados/actualizar/{id}")
+    public ResponseEntity<PartipantesMatriculados> actualizarPartipantesMatriculados(@PathVariable Integer id, @RequestBody PartipantesMatriculados partipantesMatriculados) {
+        try {
+            if (participantesMatriculadosService.findById(id) == null) {
+                return ResponseEntity.notFound().build();
+            }
+            partipantesMatriculados.setEstadoParticipanteActivo(partipantesMatriculados.getEstadoParticipanteActivo());
+            partipantesMatriculados.setEstadoParticipanteAprobacion(partipantesMatriculados.getEstadoParticipanteAprobacion());
+            return new ResponseEntity<>(participantesMatriculadosService.save(partipantesMatriculados), HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
