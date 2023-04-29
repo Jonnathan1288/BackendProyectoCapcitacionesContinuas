9359feb1bbf781cd839f93f2121b1306f3c6f9fe
EstebanBacuilima <davidbacuilima13@gmail.com>
2023-04-29 17:48:19 -0500

    nuevos
    Cambios

--- src/main/java/com/capacitaciones/continuas/services/InscritoService.java
+++ src/main/java/com/capacitaciones/continuas/services/InscritoService.java
@@ -2,5 +2,10 @@ package com.capacitaciones.continuas.services;
 
 import com.capacitaciones.continuas.models.Inscrito;
 
+import java.util.List;
+
 public interface InscritoService extends GenericService<Inscrito, Integer>{
+
+    List<Inscrito> findByCursoIdCurso(Integer idCurso);
+
 }
