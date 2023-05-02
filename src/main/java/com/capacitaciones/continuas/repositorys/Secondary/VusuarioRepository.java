7970b73f1a8a2f28f2ad6beec5a67064021bf962
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2023-05-02 02:59:26 -0500

    new chnges
    new chnges

--- src/main/java/com/capacitaciones/continuas/repositorys/Secondary/VusuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.capacitaciones.continuas.repositorys.Secondary;
-
-import com.capacitaciones.continuas.Modelos.Secondary.vusuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VusuarioRepository extends JpaRepository<vusuario,Integer> {
-}
--- src/main/java/com/capacitaciones/continuas/services/VusarioService.java
+++
@@ -1,7 +0,0 @@
-package com.capacitaciones.continuas.services;
-
-
-import com.capacitaciones.continuas.Modelos.Secondary.vusuario;
-
-public interface VusarioService extends GenericService<vusuario,Integer>{
-}
