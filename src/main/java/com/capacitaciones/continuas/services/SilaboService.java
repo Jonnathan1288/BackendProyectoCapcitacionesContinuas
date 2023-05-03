3de45afb0495ce94dedc8f4672b2f369e6962309
EstebanBacuilima <davidbacuilima13@gmail.com>
2023-05-02 22:23:11 -0500

    SIU
    na

--- src/main/java/com/capacitaciones/continuas/services/SilaboService.java
+++ src/main/java/com/capacitaciones/continuas/services/SilaboService.java
@@ -3,4 +3,10 @@ package com.capacitaciones.continuas.services;
 import com.capacitaciones.continuas.Modelos.Primary.Silabo;
 
 public interface SilaboService extends GenericService<Silabo,Integer>{
+
+    Boolean findByCursoIdCurso(Integer idCurso);
+
+
+    Silabo SilabofindByCursoIdCurso(Integer idCurso);
+
 }
