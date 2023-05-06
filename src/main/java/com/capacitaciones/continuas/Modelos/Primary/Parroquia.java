95a67bdb970bd32c3c36fe9653ead6659ff8c84b
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2023-05-06 09:15:17 -0500

    x
    x

--- src/main/java/com/capacitaciones/continuas/Modelos/Primary/Parroquia.java
+++ src/main/java/com/capacitaciones/continuas/Modelos/Primary/Parroquia.java
@@ -10,17 +10,21 @@ import java.util.List;
 @Setter
 @Getter
 @Entity
-@Table(name = "provincias")
-public class Provincia {
+@Table(name = "parroquias")
+public class Parroquia {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_provincia")
-    private Integer idProvincia;
+    private Integer idParroquia;
 
-    @Column(name = "provincia ")
-    private String provincia;
+    @Column(name = "parroquia")
+    private String parroquia;
+
+    @ManyToOne
+    @JoinColumn(name = "id_canton", referencedColumnName = "id_canton")
+    private Canton canton;
 
     @JsonIgnore
-    @OneToMany(mappedBy = "provincia")
-    private List<Canton> cantons;
+    @OneToMany(mappedBy = "parroquia")
+    private List<Curso> cursoList;
 }
--- src/main/java/com/capacitaciones/continuas/controllers/ParticipanteMatriculadosController.java
+++ src/main/java/com/capacitaciones/continuas/controllers/ParticipanteMatriculadosController.java
@@ -66,7 +66,7 @@ public class ParticipanteMatriculadosController {
                 }
                 try {
                     Curso curso = cursoService.findById(idCurso);
-                    curso.setIniciocurso(true);
+                    //curso.setIniciocurso(true);
                     curso.setEstadoPublicasionCurso("I");
                     cursoService.save(curso);
                 } catch (Exception e) {
