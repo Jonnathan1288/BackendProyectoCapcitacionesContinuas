e6a2c844cd19045aff8af907a1c45beee7950207
Diana Zhizhin <dzhizhin632@gmail.com>
2023-09-13 17:48:16 -0500

    actualizaciones
    reportesF

--- src/main/java/com/capacitaciones/continuas/Modelos/Primary/Usuario.java
+++ src/main/java/com/capacitaciones/continuas/Modelos/Primary/Usuario.java
@@ -30,7 +30,7 @@ public class Usuario {
     @Column(name = "password")
     private String password;
 
-    @Column(name = "foto_perfil", columnDefinition = "LONGBLOB")
+    @Column(name = "foto_perfil")
     private String fotoPerfil;  // igamen para la base de datos
 
     @Column(name = "estado_usuario_activo", columnDefinition = "BOOLEAN")
